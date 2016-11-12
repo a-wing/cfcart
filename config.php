f36b4431a1ad5b8ce87f78b55426c1a2536de5e5
Yongxin Shi <shiyongxin@aliyun.com>
2016-11-12 08:29:30 +0800

    add mycncart-1.6

--- config.php
+++ config.php
@@ -6,23 +6,23 @@ define('HTTP_SERVER', 'http://localhost/');
 define('HTTPS_SERVER', 'http://localhost/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/html/catalog/');
-define('DIR_SYSTEM', '/var/www/html/system/');
-define('DIR_IMAGE', '/var/www/html/image/');
-define('DIR_LANGUAGE', '/var/www/html/catalog/language/');
-define('DIR_TEMPLATE', '/var/www/html/catalog/view/theme/');
-define('DIR_CONFIG', '/var/www/html/system/config/');
-define('DIR_CACHE', '/var/www/html/system/storage/cache/');
-define('DIR_DOWNLOAD', '/var/www/html/system/storage/download/');
-define('DIR_LOGS', '/var/www/html/system/storage/logs/');
-define('DIR_MODIFICATION', '/var/www/html/system/storage/modification/');
-define('DIR_UPLOAD', '/var/www/html/system/storage/upload/');
+define('DIR_APPLICATION', 'C:/AppServ/www/catalog/');
+define('DIR_SYSTEM', 'C:/AppServ/www/system/');
+define('DIR_IMAGE', 'C:/AppServ/www/image/');
+define('DIR_LANGUAGE', 'C:/AppServ/www/catalog/language/');
+define('DIR_TEMPLATE', 'C:/AppServ/www/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/AppServ/www/system/config/');
+define('DIR_CACHE', 'C:/AppServ/www/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/AppServ/www/system/storage/download/');
+define('DIR_LOGS', 'C:/AppServ/www/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/AppServ/www/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/AppServ/www/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mpdo');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '123456');
-define('DB_DATABASE', 'cfcart_15');
+define('DB_PASSWORD', '12345678');
+define('DB_DATABASE', 'cfcart_16');
 define('DB_PORT', '3306');
-define('DB_PREFIX', 'cf_');
+define('DB_PREFIX', 'mcc_');
