d05ac4fed20e172649a23b62dc359948299839eb
YongXin Shi <shiyongxin@aliyun.com>
2016-11-26 10:44:08 +0800

    Featured language

---
+++ catalog/language/en-gb/extension/module/crowdfunding.php
@@ -0,0 +1,13 @@
+<?php
+
+//fundingtotal
+$_['text_fundingtotal']      = 'had already been pledged';
+
+//target
+$_['text_target']      = 'Goal';
+
+
+//currency
+$_['currency']  =	'$';
+$_['text_day']		=	' day';
+$_['day_remaining'] =  'Days Remaining ';
